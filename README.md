dd2e3c1fd88c1353c69b0dc12e3c6783c6890361
sriraksharao <raosri1611@gmail.com>
2025-02-22 23:51:22 -0800

    initial commit

--- README.md
+++ README.md
@@ -10,3 +10,6 @@ What we do:
 2) Activate - source ai/bin/activate
 3) Install all dependencies mentioned in requirements.txt file using the command - pip3 install -r requirements.txt
 4) Download Ollama from - https://github.com/ollama/ollama?tab=readme-ov-file
+
+Make an account in https://cloud.llamaindex.ai/project/142223a4-cc01-48f3-916c-0d7c00397fdb
+
--- main.py
+++ main.py
@@ -2,10 +2,33 @@
 # from llama_index.llms.ollama import Ollama
 
 from llama_index.llms.ollama import Ollama
+from llama_parse import LlamaParse
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, PromptTemplate
+from llama_index.core.embeddings import resolve_embed_model
 
-llm = Ollama(model="mistral", request_timeout=30.0)
-# llm = Ollama(
-#     model = "mistral", request_timeout=30.0
-# )
-result = llm.complete("hello")
+# parse pdf into some structured data
+# then convert it to Vectorestoreindex 
+# llm will utilise this db and extract just the info it needs to ans a specific query
+# vector embeddings take textual data and they embed iinto multi d space that allows us to query based on things like sentiment etc
+
+parser = LlamaParse(result_type="markdown")
+# llama parse takes docs and push them to cloud will then be parsed and parsing will be returned to us
+
+file_extractor = {".pdf": parser}
+documents = SimpleDirectoryReader("./data", file_extractor=file_extractor).load_data()
+# takes files and loads and uses appropriate file extractor
+# pass these docs to vector store index and create embeddings
+embed_model = resolve_embed_model("local:BAAI/bge-m3")
+vector_index = VectorStoreIndex.from_documents(documents, embed_model=embed_model)
+query_engine = vector_index.as_query_engine(llm=llm)
+
+result = query_engine.query("routes in api?")
 print(result)
+
+
+# llm = Ollama(model="mistral", request_timeout=30.0)
+# # llm = Ollama(
+# #     model = "mistral", request_timeout=30.0
+# # )
+# result = llm.complete("hello")
+# print(result)
